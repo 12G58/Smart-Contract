27864978f281c90086f999e7b302fd2208407dcf
free <sasha.kuhu@gmail.com>
2024-10-20 11:22:42 +0530

    Your commit message

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,26 +1,3 @@
-
-
-// require("@nomicfoundation/hardhat-toolbox");
-// // require("@chainlink/contracts"); // For Chainlink VRF
-
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: {
-//     version: "0.8.27",  // Set your primary Solidity version
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200
-//       }
-//     }, 
-//   },
-//   networks: {
-//     hardhat: {
-//       chainId: 1337, // Local network for development
-//     },
-//   },
-// };
-
 require("@nomicfoundation/hardhat-toolbox");
 // require("@chainlink/contracts"); // Uncomment if using Chainlink VRF
 
